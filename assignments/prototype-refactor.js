b085e792a8b6f9dfbc4bf4d7aed9988de5ff1ed7
oredola <oredolaolamide@gmail.com>
2019-10-17 13:01:21 +0100

    Refactored the constructor function for Task 1

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -7,3 +7,24 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+
+  eat(edible) {
+    if (this.stomach.length < 10) {
+      return this.stomach.push(edible);
+    }
+  }
+
+  poop() {
+    return (this.stomach = []);
+  }
+
+  toString() {
+    return `${this.name}, ${this.age}`;
+  };
+}
