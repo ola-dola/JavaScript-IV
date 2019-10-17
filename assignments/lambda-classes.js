dbe6e40cccde639153a076c4506b2e6d628c440a
oredola <oredolaolamide@gmail.com>
2019-10-17 14:01:48 +0100

    Added the Person base class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,24 @@
 // CODE here for your Lambda Classes
+
+//Person class.
+
+class Person {
+  constructor(name, age, location) {
+    this.name = name;
+    this.age = age;
+    this.name = name;
+    this.location = location;
+  }
+
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
+}
+//Person class test.
+const bali = new Person('Tope', 23, 'Ibadan');
+const bili = new Person('Kofe', 25, 'London');
+const ed = new Person('Ed', 32, 'Oak');
+
+console.log(bali.speak());
+console.log(bili.speak());
+console.log(ed.speak());
