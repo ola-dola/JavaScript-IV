51e15e39c8a6b67df09abbb2f08acaeafed79434
oredola <oredolaolamide@gmail.com>
2019-10-17 14:34:03 +0100

    Added an Instructor class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -21,3 +21,44 @@ const ed = new Person({ name: 'Ed', age: 63, location: 'Lagos' });
 console.log(bali.speak());
 console.log(bili.speak());
 console.log(ed.speak());
+
+//Instructor class
+
+class Instructor extends Person {
+  constructor(attr) {
+    super(attr);
+    this.specialty = attr.specialty;
+    this.favLanguage = attr.favLanguage;
+    this.catchPhrase = attr.catchPhrase;
+  }
+
+  demo(subject) {
+    console.log(`Today we are learning about ${subject}`);
+  }
+
+  grade(student, subject) {
+    console.log(`${student.name} receives a perfect score on ${subject}`)
+  }
+}
+//Instructor test
+
+const gabe = new Instructor({
+  name: 'Gabe', 
+  age: 35, 
+  location: 'Spain',
+  specialty: 'Redux',
+  favLanguage: 'Javascript',
+  catchPhrase: 'I will test you!'
+});
+
+const alex = new Instructor({
+  name: 'Alex',
+  age:29,
+  location: 'UK',
+  specialty: 'Frontend',
+  favLanguage: 'Javascript',
+  catchPhrase: 'Dabba do!'
+});
+
+alex.demo('react');
+gabe.grade({name: 'Ola'}, 'Napping');
